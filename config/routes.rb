21340ec3855705f910b4748df80c47e0320e9254
Dustin Grof <dustingrof@gmail.com>
2022-05-04 18:33:53 -0700

    Add category resource to admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+  end
   get 'about/index'
   get 'static_pages/help'
   get 'static_pages/faq'
@@ -19,6 +22,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :delete]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
